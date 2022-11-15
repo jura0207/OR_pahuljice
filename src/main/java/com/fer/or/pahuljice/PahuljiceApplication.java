16f197d42c62527a7c89f6011d51b083831c2a43
Jura Vuković <jv51207@fer.hr>
2022-11-15 08:04:16 +0100

    Initial commit for lab02

---
+++ src/main/java/com/fer/or/pahuljice/PahuljiceApplication.java
@@ -0,0 +1,13 @@
+package com.fer.or.pahuljice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PahuljiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PahuljiceApplication.class, args);
+	}
+
+}
